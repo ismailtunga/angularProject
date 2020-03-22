d300de55c3f6cc26e4d4d38c6890e8a75755e06a
İsmail TUNGA <i.tunga@hotmail.com>
2020-03-23 02:28:03 +0300

    route

--- movieApp/src/app/movie.service.ts
+++ movieApp/src/app/movie.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Movie } from './movie';
 import { Movies } from './movie.datasource';
 import { Observable, of } from 'rxjs';
+import { LoggingService } from './logging.service';
 
 
 @Injectable({
@@ -9,9 +10,10 @@ import { Observable, of } from 'rxjs';
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) { }
 
   getMovies(): Observable<Movie[]>{
+    this.loggingService.add('MovieService: listing movies');
     return of(Movies);
   }
 }
